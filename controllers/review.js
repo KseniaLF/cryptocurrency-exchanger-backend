724f1e49b626c4e3613fb0760898f49bdc8ca13d
KseniaLF <llf9391@gmail.com>
2023-08-27 18:06:45 +0300

    add review endpoints to swagger

--- controllers/review.js
+++ controllers/review.js
@@ -9,10 +9,10 @@ const addReview = async (req, res, next) => {
 
   const updatedReview = await Review.findOneAndUpdate({ owner }, body, {
     new: true,
-  });
+  }).select("-owner");
 
   if (!updatedReview) {
-    const newReview = await Review.create({ ...body, owner });
+    const newReview = await Review.create({ ...body, owner }).select("-owner");
     res.status(201).json(newReview);
   } else {
     res.status(200).json(updatedReview);
@@ -22,10 +22,8 @@ const addReview = async (req, res, next) => {
 const getReview = async (req, res, next) => {
   const { _id: owner } = req.user;
 
-  const review = await Review.findOne({ owner: owner }).populate(
-    "owner",
-    "email name"
-  );
+  const review = await Review.findOne({ owner: owner });
+  // .populate("owner", "email name");
 
   res.status(200).json(review);
 };
