0482003ea3749f40e9c6642f06530f1741aa5b3f
Vlad Koblai <vkchabraww@gmail.com>
2023-08-28 10:42:56 +0300

    Update transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -2,21 +2,6 @@ const { Schema, model } = require("mongoose");
 
 const transactionSchema = new Schema(
   {
-    userFullName: {
-      type: String,
-      required: [true, "Write your full name"],
-    },
-    email: {
-      type: String,
-      required: [true, "Write your email"],
-      unique: [true, "Email must be unique"],
-    },
-    telegram: {
-      type: String,
-    },
-    number: {
-      type: String,
-    },
     currencyToExchange: {
       type: String,
       enum: ["curr1", "curr2"], // need full list of possible currencies here
