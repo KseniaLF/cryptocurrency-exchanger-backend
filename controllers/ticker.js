4f94377071db84bbebb31163061b0fa69dac214f
VKChabra <vkchabraww@gmail.com>
2023-10-06 14:18:34 +0300

    Update ticker.js

--- controllers/ticker.js
+++ controllers/ticker.js
@@ -7,29 +7,31 @@ const getTicker = async (req, res, next) => {
       "https://whitebit.com/api/v4/public/ticker"
     );
     const tickerData = response.data;
+    // console.log(tickerData.ETH_USD, tickerData.ETH_USDT, tickerData.ETH_USD);
 
     const cryptoList = [];
 
-    // Проходимся по всем данным о криптовалютах
+    const usdtToUsd = tickerData.USDT_USD.last_price;
     for (const pairSymbol in tickerData) {
       const pairData = tickerData[pairSymbol];
       const [crypto, quoteCurrency] = pairSymbol.split("_");
 
-      // Проверяем, что это пара с котируемой валютой USDT
-      if (quoteCurrency === "USDT") {
-        const priceInUSDT = parseFloat(pairData.last_price);
+      if (quoteCurrency === "USDT" || pairSymbol === "USDT_USD") {
+        // const priceInUSDT = parseFloat(pairData.last_price);
+        const priceInUSD = parseFloat(pairData.last_price * usdtToUsd);
         const change24h = parseFloat(pairData.change);
+        const marketCap = Math.round(pairData.quote_volume);
 
-        // Рассчитываем рыночную капитализацию (примерно)
-        // Это просто для иллюстрации исходя из текущей цены и объема
-        const marketCap = Math.round(priceInUSDT * pairData.quote_volume);
-
-        // Добавляем информацию о криптовалюте в список
-        cryptoList.push({ crypto, priceInUSDT, change24h, marketCap });
+        cryptoList.push({
+          crypto,
+          // priceInUSDT,
+          priceInUSD,
+          change24h,
+          marketCap,
+        });
       }
     }
 
-    // Сортируем криптовалюты по рыночной капитализации в порядке убывания
     cryptoList.sort((a, b) => b.marketCap - a.marketCap);
 
     const page = req.query.page || 1;
@@ -40,8 +42,8 @@ const getTicker = async (req, res, next) => {
 
     res.json(paginatedCryptoList);
   } catch (error) {
-    console.error("Ошибка при запросе к API:", error);
-    res.status(500).json({ error: "Ошибка при запросе к API" });
+    console.error("Error fetching Api:", error);
+    res.status(500).json({ error: "Error fetching Api" });
   }
 };
 
